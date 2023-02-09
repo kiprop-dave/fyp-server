ef802b28b4fa2299c8f9a346dc724804561f3bc1
kiprop-dave <davekip16@gmail.com>
2023-02-09 09:47:43 +0300

    added production build origin

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,5 +1,6 @@
 const allowedOrigins: string[] = [
   "http://localhost:5173",
+  "http://localhost:4173",
   "http://127.0.0.1:5173",
   "https://fyp-web-client.vercel.app",
 ];
--- src/config/mqttConfig.ts
+++ src/config/mqttConfig.ts
@@ -3,7 +3,7 @@ import mqtt from "mqtt";
 // Set up MQTT client
 const tcpUrl = process.env.MQTT_URL || "mqtt://localhost:1883";
 const connectionOptions: mqtt.IClientOptions = {
-  clientId: "mqttjs01",
+  clientId: "node-server",
   username: process.env.MQTT_USERNAME,
   password: process.env.MQTT_PASSWORD,
   clean: true,
